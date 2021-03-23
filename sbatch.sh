5e19e78f274dbc91c97774f81bae43c312443209
Jia Liu <jialiu@berkeley.edu>
2021-03-23 13:14:25 -0700

    update untar

--- sbatch.sh
+++ sbatch.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=32
+#SBATCH --tasks-per-node=68
+###32
 #SBATCH --cpus-per-task=1
-#SBATCH -C haswell
-#SBATCH -t 2:00:00
+#SBATCH -C knl
+##haswell
+#SBATCH -t 00:30:00
 #SBATCH -J cosmos_kappa
 #SBATCH -o cosmos_kappa.o%j
 #SBATCH -e cosmos_kappa.e%j
-#SBATCH --qos=regular
+#SBATCH --qos=debug
+##regular
 #SBATCH -A mp107
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jialiu@berkeley.edu
 
-source /global/common/software/m3035/conda-activate.sh 3.7
+#source /global/common/software/m3035/conda-activate.sh 3.7
 
+#module load python3
 srun python /global/homes/j/jialiu/pzCOSMOS/kappaTNG_COSMOS.py
